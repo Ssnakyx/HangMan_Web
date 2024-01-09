73a6217dcbd1cf37a43be23c55b06f6749baff0d
Ssnakyx <hamed.kaffa@ynov.com>
2024-01-09 09:28:50 +0100

    Affichage mots dans la box

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module HangMan_Web
 
 go 1.21.0
 
-require github.com/Ssnakyx/HangMan____ v0.0.0-20231219141200-0784e69381d8 // indirect
+require github.com/Ssnakyx/HangMan____ v0.0.0-20240109001233-40454829888e
--- server.go
+++ server.go
@@ -1,20 +1,60 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
-	"text/template"
+	"sync"
+
+	hangman "github.com/Ssnakyx/HangMan____"
 )
 
+var (
+	mu           sync.Mutex
+	currentWord  string
+	foundLetters []string
+)
+
+type HangManData struct {
+	WordToGuess       string
+	DisplayedWord     string
+	AttemptsRemaining int
+	GameStage         []string
+	Lettre            string
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if currentWord == "" {
+		words, err := hangman.ReadWordsFromFile("words.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		currentWord = hangman.GetRandomWordFromList(words)
+	}
+
+	data := HangManData{
+		WordToGuess:       currentWord,
+		AttemptsRemaining: 6,
+		GameStage:         []string{},
+		Lettre:            "",
+		DisplayedWord:     hangman.DisplayWord(currentWord, foundLetters),
+	}
+
 	template, err := template.ParseFiles("HTMLL/forms.html", "HTMLL/footer.html", "HTMLL/header.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-	template.Execute(w, nil)
+	template.Execute(w, data)
 }
 
 func FormulaireHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+
 	if r.Method == "POST" {
 		err := r.ParseForm()
 		if err != nil {
@@ -23,11 +63,22 @@ func FormulaireHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		lettre := r.FormValue("Lettre")
-		log.Print(lettre)
+		foundLetters = append(foundLetters, lettre)
 
-		data := struct {
-			Lettre string
-		}{Lettre: lettre}
+		data := HangManData{
+			WordToGuess:       currentWord,
+			AttemptsRemaining: 6,
+			GameStage:         []string{},
+			Lettre:            lettre,
+		}
+
+		data.DisplayedWord = hangman.DisplayWord(currentWord, foundLetters)
+
+		if hangman.IsWordGuessed(currentWord, foundLetters) {
+
+			http.Redirect(w, r, "/victoire", http.StatusFound)
+			return
+		}
 
 		template, err := template.ParseFiles("HTMLL/forms.html", "HTMLL/footer.html", "HTMLL/header.html")
 		if err != nil {
@@ -38,13 +89,21 @@ func FormulaireHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, "http://localhost:8080", http.StatusFound)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func VictoirePage(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("HTMLL/victoire.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, nil)
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/hangman", FormulaireHandler)
-
+	http.HandleFunc("/victoire", VictoirePage)
 	fs := http.FileServer(http.Dir("CSS/"))
 	http.Handle("/CSS/", http.StripPrefix("/CSS", fs))
 	log.Println("Serveur allumé")
